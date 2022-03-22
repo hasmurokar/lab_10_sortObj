87dd235c8b2c071485304640027f6276ffc42a33
margaritakaiki@outlook.com <margaritakaiki@outlook.com>
2022-03-22 21:18:22 +0200

    Добавьте файлы проекта.

---
+++ task1/CityBuild.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class CityBuild: IComparable<CityBuild>
+    {
+        public string CompanyName { get; set; }
+        public string BuildObject { get; set; }
+        public int Square { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string State { get; set; }
+
+        public CityBuild(string companyName, string buildObject, int square, DateTime startDate, DateTime endDate, string state)
+        {
+            CompanyName = companyName;
+            BuildObject = buildObject;
+            Square = square;
+            StartDate = startDate;
+            EndDate = endDate;
+            State = state;
+        }
+        public int GetDays()
+        {
+            return EndDate.Subtract(StartDate).Days;
+        }
+        public override string ToString()
+        {
+            return $"{CompanyName,-12} | {BuildObject,-12} | {Square,5}м^2 | {StartDate.ToString("d"),-9} | {EndDate.ToString("d"),-12} | {State,-10} | {GetDays()} д.";
+        }
+
+        public int CompareTo(CityBuild other)
+        {
+            return CompanyName.CompareTo(other.CompanyName);
+        }
+
+        public class SortByBuildObject : IComparer<CityBuild>
+        {
+            public int Compare(CityBuild x, CityBuild y)
+            {
+                return x.BuildObject.CompareTo(y.BuildObject);
+            }
+        }
+
+        public class SortBySquare : IComparer<CityBuild>
+        {
+            public int Compare(CityBuild x, CityBuild y)
+            {
+                return x.Square.CompareTo(y.Square);
+            }
+        }
+
+        public class SortByStartDate : IComparer<CityBuild>
+        {
+            public int Compare(CityBuild x, CityBuild y)
+            {
+                return x.StartDate.CompareTo(y.StartDate);
+            }
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task1
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            var list = new List<CityBuild>();
+            for (int i = 1; i <= 5; i++)
+            {
+                var cName = "Building " + rnd.Next(1, 9);
+                var bObject = "Object " + rnd.Next(1, 9);
+                var square = rnd.Next(20, 100);
+                var sDate = RandomDay();
+                var eDate = RandomDay().AddYears(rnd.Next(5, 7));
+                var state = "No Info";
+                list.Add(new CityBuild(cName, bObject, square, sDate, eDate, state));
+            }
+
+            Outputer(list);
+            list.Sort();
+            Console.WriteLine("Сортировка по названию компании");
+            Outputer(list);
+            Console.WriteLine("Сортировка по названию здания");
+            list.Sort(new CityBuild.SortByBuildObject());
+            Outputer(list);
+            Console.WriteLine("Сортировка по площади строения");
+            list.Sort(new CityBuild.SortBySquare());
+            Outputer(list);
+            Console.WriteLine("Сортировка по дате начала строительства");
+            list.Sort(new CityBuild.SortByStartDate());
+            Outputer(list);
+
+        }
+        public static DateTime RandomDay()
+        {
+            var start = new DateTime(2018, 1, 1);
+            var range = (DateTime.Today - start).Days;
+            return start.AddDays(rnd.Next(range));
+        }
+
+        private static void Outputer(List<CityBuild> builds)
+        {
+            Console.WriteLine("Название     |Объект        |Площадь   |Дата начала |Дата окончания|Состояние   | Остаток дней");
+            foreach (var item in builds)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+
+        public static bool IsEndFourQurtal(DateTime endDate)
+        {
+            var startQ = new DateTime(2022, 10, 1);
+            var endQ = new DateTime(2022, 12, 31);
+            if (endDate > startQ && endDate < endQ)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
